368c762e0dfb42870fd97d441ee465c5b107e647
Jarom Roney <byutrueblue@gmail.com>
2018-02-16 21:09:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,16 +10,20 @@ function updateObjectWithKeyAndValue(object, key, value)
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 {
  object[key] = value 
+ return object
 }
 
 function deleteFromObjectByKey(object, key)
 {
   delete object.key
+
+  return object
 }
 
 function destructivelyDeleteFromObjectByKey(object, key)
 {
   delete object.key
+  return object
 }
 
 
