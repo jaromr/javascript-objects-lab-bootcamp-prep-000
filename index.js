ff47bdab9e07d92e44a677d2fc9a8b21b3d0f1a5
Jarom Roney <byutrueblue@gmail.com>
2018-02-16 21:08:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,26 +16,12 @@ function deleteFromObjectByKey(object, key)
   delete object.key
 }
 
-function 
+function destructivelyDeleteFromObjectByKey(object, key)
+{
+  delete object.key
+}
+
 
   
 
  
-
-  describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
-    it('returns object without the delete key/value pair', function() {
-      var obj = { prop: 1 }
-      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-      expect(newObj['prop']).toBe(undefined)
-    })
-
-    it('modifies the original object', function() {
-      var obj = { prop: 1 }
-      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-      expect(obj['prop']).toBe(undefined)
-    })
-  })
-
-})
